f0481e16b652f2e7d92bf72cab3a9987ba75f81c
raghav <1raghuram3@gmail.com>
2017-02-11 19:33:01 +0530

    Node class

---
+++ NodeClassForStockData.java
@@ -0,0 +1,17 @@
+package com.Bridgelabz;
+
+public class NodeClassForStockData 
+{
+	Object StockData;
+	NodeClassForStockData next;
+
+public NodeClassForStockData(StockData ob) {
+	
+	StockData=ob;
+next=null;
+
+}
+
+	
+
+}
