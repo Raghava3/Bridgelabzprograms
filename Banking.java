16587c86f5d69b793e1499a90721e703c87777c7
raghav <1raghuram3@gmail.com>
2017-02-21 10:19:17 +0530

    updated

--- Banking.java
+++ Banking.java
@@ -22,8 +22,8 @@ public class Banking
 
 			System.out.println(" enter 2  to proceed with transaction  ");
 			n=sc.next().charAt(0);
-//generating the ticket number{
-			
+			//generating the ticket number{
+
 			if(n=='1')
 			{ 
 				flag1=1;
@@ -34,7 +34,7 @@ public class Banking
 				System.out.println(l);
 			}
 
-//checking the ticket number}
+			//checking the ticket number}
 			else if(n=='2')
 			{
 				if(flag1==1)  
@@ -50,101 +50,101 @@ public class Banking
 						break;
 					}
 				}
-			//	System.out.println("tic="+tic+"k="+k+"l="+l);
-					if(tic==k&&(l>0))
+				//	System.out.println("tic="+tic+"k="+k+"l="+l);
+				if(tic==k&&(l>0))
+				{
+
+					flag=1;
+
+					while(flag==1)
 					{
 
-						flag=1;
 
-						while(flag==1)
-						{
-							
-							
-							try{
-							
+						try{
+
 							System.out.println("Select your operation in Bank\n 1>\tFor Deposite\n 2>\tFor WithDraw\n 3>\tFor Check Balance\n 4>\tExit\nEnter Your choice!!!");
 							option =sc.nextInt();
-							}
-							catch(InputMismatchException f)
+						}
+						catch(InputMismatchException f)
+						{
+							while (true)
 							{
-								while (true)
-								{
-								
+
 								System.out.println(" transaction incomplete . join queue for any further trancation ");
 								option =4;
 								break;
+							}
+						}
+						if(option==1)
+						{ 
+							try{
+								System.out.println("Enter the amount to deposite");
+								amtd=sc.nextDouble();
+							}
+							catch(InputMismatchException d)
+							{
+								while(true)
+								{   flag=0;
+								fl=0;
+								System.out.println(" invalid amount,transaction incomplete . join queue for any further trancation");
+								tic++;
+								l=q.exit();
+								break;
 								}
 							}
-							if(option==1)
-								{ 
-								try{
-									System.out.println("Enter the amount to deposite");
-									 amtd=sc.nextDouble();
-								}
-								catch(InputMismatchException d)
-								{
-									while(true)
-									{   flag=0;
-										fl=0;
-										System.out.println(" invalid amount,transaction incomplete . join queue for any further trancation");
-										tic++;
-										l=q.exit();
-									    break;
-								    }
-								}
-								if(fl==1)
-								{
+							if(fl==1)
+							{
 								b.dep(amtd);
-									q.exit();
+								q.exit();
 
-								}
-								}
+							}
+						}
 
-								else if(option==2)
-								{
-                                   try{
-									System.out.println("Enter the amount to withdraw");
-									amtw=sc.nextDouble();
-                                   }
-                                   catch(InputMismatchException a)
-                                   {
-                                	   while(true)
-   									{
-                                		   flag=0;
-   										fl1=0;
-   										System.out.println(" invalid amount. transaction incomplete . join queue for any further trancation  ");
-   										tic++;
-   										l=q.exit();
-   									    break;
-   								    }
-                                   }
-                                   if(fl1==1)
-                                   {
-                                   b.wd(amtw);
-									q.exit();
-                                   }
-								}
-								else if(option ==3)
-								{
-									b.check();
-
-								}
-								else if(option ==4)
+						else if(option==2)
+						{
+							try{
+								System.out.println("Enter the amount to withdraw");
+								amtw=sc.nextDouble();
+							}
+							catch(InputMismatchException a)
+							{
+								while(true)
 								{
 									flag=0;
+									fl1=0;
+									System.out.println(" invalid amount. transaction incomplete . join queue for any further trancation  ");
 									tic++;
 									l=q.exit();
-									System.out.println("thank u . ");
+									break;
 								}
+							}
+							if(fl1==1)
+							{
+								b.wd(amtw);
+								q.exit();
+							}
+						}
+						else if(option ==3)
+						{
+							b.check();
 
+						}
+						else if(option ==4)
+						{
+							flag=0;
+							tic++;
+							l=q.exit();
+							System.out.println("thank u . ");
 						}
 
 					}
 
-					else 
-					{
-						System.out.println("please come in queue ");
-					}
+				}
+
+				else 
+				{
+					System.out.println("please come in queue ");
+				}
 
 				}
 
