29c455fdca8cba1cd34f391e7ab8a98fd1a5843c
raghav <1raghuram3@gmail.com>
2017-02-23 12:54:13 +0530

    this is ordered list in ascending order uses Linked list

--- TestListInt.java
+++ TestListInt.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class TestListInt {
 	public static void main(String[] args) throws IOException {
-		//reading the file
+		//reading from the file
 		FileReader fs=new FileReader("/home/bridgelabz3/Desktop/Raghava/raghava/Fileint.txt");
 		
 		BufferedReader br =new BufferedReader(fs);
@@ -24,22 +24,25 @@ public class TestListInt {
 		for (int i = 0; i < str.length; i++) {
 			arr[i]=Integer.parseInt(str[i]);
 		}
-		//sort the array in ascending order
+		//sort the array in ascending order using utility class 
 		UtilityClass ut=new UtilityClass();
 		arr=ut.bubbleSortForInteger(arr);
-		/*
+		
 		for (int i = 0; i < arr.length; i++) {
 			ll.add(arr[i]);
-		}*/
-	//	System.out.println(ll);
+		}
+		System.out.println(ll);
 		System.out.println("enter a no to find.....");
 		Scanner scn=new Scanner(System.in);
 		int find=scn.nextInt();
 		scn.close();
+		//calling the method which is present in linked list
 		if(ll.search(find)){
 			ll.remove(find);
+			ll.removeAll(find);
 			System.out.println("value found....so..!!!!! Removed");
 		}
+		//calling the method which is present in linked list
 		else{
 			ll.add(find);
 			System.out.println("value not found....so..!!!!! Added");
