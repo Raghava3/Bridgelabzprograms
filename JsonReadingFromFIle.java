92e870685238a215156ba3965444235e017e00c3
raghav <1raghuram3@gmail.com>
2017-02-24 11:37:08 +0530

    updated

--- JsonReadingFromFIle.java
+++ JsonReadingFromFIle.java
@@ -2,22 +2,23 @@ package com.Bridgelabz;
 
 import java.io.FileReader;
 import java.util.Iterator;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class JsonReadingFromFIle {
 public static void main(String[] args) throws Exception{
-		
+		//Reading from the file 
 		FileReader file=new FileReader("/home/bridgelabz3/Desktop/Raghava/raghava/FILE1.txt");
+		//creating the object of jsonparser 
 		JSONParser parser= new JSONParser();
-		
+		//reading the file using the parser methods
 		Object obj=parser.parse(file);
+		//passing that to object 
 		JSONObject jo=(JSONObject)obj;
 		JSONArray ja=(JSONArray)jo.get("rice");
 		Iterator<String> iterator ;
-	//	System.out.println("Rice");
+
 		System.out.println();
 try{
 	 iterator = ja.iterator();
@@ -61,7 +62,7 @@ try{
 		{
 			System.out.println("there is no item");
 		}
-            System.out.println();
+         /*   System.out.println();
     		ja=(JSONArray)jo.get("ragi");
     	//	System.out.println("wheat");
     		System.out.println();
@@ -76,7 +77,7 @@ try{
     		{
     			System.out.println("there is no item");
     		}
-
+*/
 
 	}
 }
